cf46382839f297c10975f6e775287fd5420ba8d2
soof05 <sel-ouaf@student.1337.ma>
2024-05-19 23:03:20 +0100

    Handle errors

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,13 +1,18 @@
-import {Heading, Text} from '@chakra-ui/react'
+import {Box, Heading, Text} from '@chakra-ui/react'
 import { isRouteErrorResponse, useRouteError } from 'react-router-dom'
+import NavBar from '../components/NavBar'
+// import NavBar from '../components/NavBar'
 
 const ErroPage = () => {
     const error = useRouteError()
 
     return (
         <>
+            <Box padding={5}>
+            <NavBar/>
             <Heading>Ooops</Heading>
             <Text>{isRouteErrorResponse(error) ? 'Page does not exist' : 'An u nexpected error occured'}</Text>
+            </Box>
         </>
     )
 }
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,8 +4,8 @@ import {Outlet} from 'react-router-dom'
 const Layout = () => {
     return (
         <>
-            <NavBar />
-            <Outlet />
+            <NavBar/>
+            <Outlet/>
         </>
     )
 }
